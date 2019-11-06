6838175382815ffff0467ae20b8be6cdadfc4f38
Vanessa Davis <vanessamdavis@Vanessas-MacBook-Air.local>
2019-11-06 15:58:32 -0700

    making similar wines a list instead of np arr

--- src/populatedb.py
+++ src/populatedb.py
@@ -7,7 +7,7 @@ def connect():
                         user="postgres",
                         host="18.219.179.32", port="5432")
     cur = conn.cursor()
-    return cur
+    return conn, cur
 
 def insert_into_db(conn, cur, wine_id, similar_wines):
     insert_vals = [wine_id, similar_wines]
@@ -20,7 +20,7 @@ def save_similarities(conn, cur, dist_matrix):
     for idx, wine_dists in enumerate(dist_matrix):
         wine_id = idx
         similar_wines = np.argsort(wine_dists)[1:50]
-        insert_into_db(conn, cur, wine_id, similar_wines)
+        insert_into_db(conn, cur, wine_id, list(similar_wines))
 
 def close_connection(conn):
     conn.close()
