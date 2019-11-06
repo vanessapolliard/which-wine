2ffe11865d7e28fe39cae35161fb9588d536fbd3
Vanessa Davis <vanessamdavis@Vanessas-MacBook-Air.local>
2019-11-06 15:47:59 -0700

    passing cur and conn in DB functions

--- src/populatedb.py
+++ src/populatedb.py
@@ -7,26 +7,27 @@ def connect():
                         user="postgres",
                         host="18.219.179.32", port="5432")
     cur = conn.cursor()
+    return cur
 
-def insert_into_db(wine_id, similar_wines):
+def insert_into_db(conn, cur, wine_id, similar_wines):
     insert_vals = [wine_id, similar_wines]
     insert_query = "INSERT INTO similarwines VALUES \
                     (%s, %s)"
     cur.execute(insert_query, tuple(insert_vals))
     conn.commit()
 
-def save_similarities(dist_matrix):
+def save_similarities(conn, cur, dist_matrix):
     for idx, wine_dists in enumerate(dist_matrix):
         wine_id = idx
         similar_wines = np.argsort(wine_dists)[1:50]
-        insert_into_db(wine_id, similar_wines)
+        insert_into_db(conn, cur, wine_id, similar_wines)
 
-def close_connection():
+def close_connection(conn):
     conn.close()
 
 
 if __name__ == '__main__':
     # connect()
-    # save_similarities(dists)
+    # save_similarities(conn, cur, dists)
     # close_connection()
     pass
