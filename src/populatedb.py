cbd551cff0e53b857ddaff5c2def45877d6dd6c3
Vanessa Davis <vanessamdavis@Vanessas-MacBook-Air.local>
2019-11-06 15:06:07 -0700

    db connections in functions

--- src/populatedb.py
+++ src/populatedb.py
@@ -2,10 +2,11 @@ import numpy as np
 import pandas as pd
 import psycopg2
 
-conn = psycopg2.connect(database="whichwine",
+def connect():
+    conn = psycopg2.connect(database="whichwine",
                         user="postgres",
                         host="18.219.179.32", port="5435")
-cur = conn.cursor()
+    cur = conn.cursor()
 
 def insert_into_db(wine_id, similar_wines):
     insert_vals = [wine_id, similar_wines]
@@ -20,8 +21,12 @@ def save_similarities(dist_matrix):
         similar_wines = np.argsort(wine_dists)[1:50]
         insert_into_db(wine_id, similar_wines)
 
+def close_connection():
+    conn.close()
+
 
 if __name__ == '__main__':
+    # connect()
     # save_similarities(dists)
-    # conn.close()
+    # close_connection()
     pass
